9e5a883521238d71502a3669738e130e7b55779d
Vincent506 <vicenteneto0806@gmail.com>
2025-06-26 17:17:52 -0300

    code 1.0

---
+++ output/output/ListaSimples.cpp
@@ -0,0 +1,104 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+//esturtura de uma lista encadeada simples
+typedef struct no
+{
+    int valor;
+    struct no *proximo;
+}No;
+
+//Função para inserir no inicio
+//dentro da nossa struct no temos um endereço de memoria
+//para acessalo usaremos ponteiro pra ponteiro (No **lista)
+//e o valor que iremos adicionar(int num)
+void inserir_no_inicio(No **lista, int num){
+//cria se um ponteiro que vamos usar como um espaço vazio
+//utilizamos a função malloc para reservar esse espaço de memoria
+    No *novo = (No*)malloc(sizeof(No));  
+    if (novo)
+    {
+        //Dentro da nossa condição de criação desse ponteiro
+        //vamos preenche-lo. Conlocamos o "num" do paramentro 
+        //da função na variavel dentro de "valor".
+        novo->valor = num;
+        //O ponteiro de "novo" vai apontar para o ponteiro da
+        //"lista" que foi parametro da nossa função
+        novo->proximo = *lista;
+        //por fim o endereço da estrutura do parametro vai receber o 
+        //espaço de memoria que nos haviamos separado. Agora preenchido 
+        *lista = novo;
+    }else{
+        printf("Erro ao alocar memoria");
+    }
+}
+//função para mostrar a lista encadeada
+void mostrar(No *cabeca){
+//criar um ponteiro que percorre a nossa lista
+    No *percorre = cabeca;
+    //se a lista estiver vazia imprime uma mensagem 
+    if (cabeca == NULL)
+    {
+        system("clear");
+        printf("Lista vazia");
+    }
+    //se não, usamos uma estrutura de repetição while para percorrer a lista
+    //ate o ponteiro "percorre" ou seja o fim da nossa lista 
+        while (percorre != NULL)
+        {
+            //exibe o valor inteiro
+            printf("%d - ", percorre->valor);
+            //percorre recebe ele mesmo apontando para o proximo
+            percorre = percorre->proximo;
+        }
+        printf("\n");
+    }
+
+//Dentro desse programa nos temos uma lista encadeada onde so adicionamos 
+//elementos no topo da lista e exibimos.
+int main()
+{
+    No *listaSimples = NULL;
+    int option = -1;
+    while (option != 0)
+    {
+        printf("MENU DE OPÇÕES\n");
+        printf("1 - Adicionar elemento\n");
+        printf("2 - Mostrar a Lista\n");
+        printf("0 - Sair e fechar o programa\n");
+        printf("O que deseja fazer? ");
+        scanf("%d", &option);
+        
+        switch (option)
+        {
+        case 1:
+        system("clear");
+            printf("Digite um numero inteiro: ");
+            int elemento;
+            scanf("%d",&elemento);
+            inserir_no_inicio(&listaSimples, elemento);
+            
+            printf("\nValor %d adicionado com sucesso!!\n\n",elemento);
+            break;
+
+            case 2:
+            system("clear");
+            printf("Lista simplesmente encadeada:\n");
+            mostrar(listaSimples);
+            break;
+            case 0:
+            system("clear");
+            printf("fechando o programa....\n");
+            break;
+        
+        default:
+        system("clear");
+            printf("Digite uma opção valida!!\n");
+            break;
+        }
+    }
+    return 0;
+}
+
+
+
