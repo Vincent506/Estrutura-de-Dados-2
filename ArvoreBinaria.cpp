4013d2b7bb7e827fba48815094eab433813ee117
Vincent506 <vicenteneto0806@gmail.com>
2025-07-01 10:53:38 -0300

    demo 1.0

--- ArvoreBinaria.cpp
+++ ArvoreBinaria.cpp
@@ -19,15 +19,79 @@ void criar_arvore(Tree *arvore){
     arvore->raiz = NULL;
 }
 
-void criar_raiz(Tree *arvore, int valor){
-    arvore->raiz->direita = NULL;
-    arvore->raiz->esquerda = NULL;
-    arvore->raiz->valor = valor;
-    arvore->quantidade++;
+No *cria_no(){
+    No *novo = new no;
+    return novo;
+}
+
+
+void inserir(Tree *arvore, int num){
+    No *novo = new No;
+    No *aux = new No;
+    if (novo)
+    {
+        novo->valor = num;
+        if (arvore->raiz == NULL)
+        {
+            arvore->raiz = novo->direita;
+        }else if (arvore->raiz != NULL && arvore->raiz->direita)
+        {
+            aux = arvore->raiz->direita;
+            while (aux->direita != NULL)
+            {
+                if (aux->valor>num)
+                {
+                    aux = aux->direita;
+                }else{
+                    novo->direita = aux->direita;
+                break;
+                }
+            }
+        }else if (arvore->raiz != NULL && arvore->raiz->esquerda)
+        {
+            //verificação do numero menor na arvore terminar depois
+        }
+        
+        arvore->quantidade++;
+    }else{
+        printf("\tErro ao alocar memoria\n");
+    }
+    
 }
 
 int main()
 {
-    printf("hello word");
+    Tree ArvoreBinaria;
+    criar_arvore(&ArvoreBinaria);
+
+    int option = -1;
+    int elemento;
+    while (option != 0)
+    {
+        printf("MENU DE OPÇOES\n");
+        printf("1 - Inserir elemento\n");
+        printf("2 - Mostrar Arvore\n");
+        printf("3 - Buscar\n");
+        printf("0 - sair\n");
+        printf("Digite a sua opção: ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+        system("clear");
+            printf("Digite o elemento: ");
+            scanf("%d",&elemento);
+            inserir(&ArvoreBinaria, elemento);
+            break;
+        case 0:
+        system("clear");
+            printf("Saindo e fechando o programa....\n");
+        break;
+        default:
+        printf("Digite uma opção valida");
+            break;
+        }
+    }
+    
     return 0;
 }
