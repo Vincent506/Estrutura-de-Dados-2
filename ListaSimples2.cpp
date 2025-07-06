41d1dfc10b2f47c56902022266baee7ceb9460f3
Vincent506 <vicenteneto0806@gmail.com>
2025-07-06 20:10:33 -0300

    code 1.1

--- ListaSimples2.cpp
+++ ListaSimples2.cpp
@@ -46,6 +46,30 @@ void inserir_no_final(No **lista, int elemento){
     }
 }
 
+void inserir_no_meio(No **lista,int anterior, int elemento){
+    No *novo = new No;
+    if (novo)
+    {
+        novo->elemento = elemento;
+        if (lista == NULL)
+        {
+            novo->proximo = NULL;
+            *lista = novo;
+        }else{
+            No *aux = new No;
+            aux = *lista;
+            while (aux->proximo != NULL && aux->elemento != anterior)
+            {
+                aux = aux->proximo;
+            }
+            novo->proximo = aux->proximo;
+            aux->proximo = novo;   
+        }
+    }else{
+        printf("Erro ao alocar memoria");
+    }
+}
+
 void mostrar(No*lista){
     No *aux = lista;
     
@@ -57,9 +81,9 @@ void mostrar(No*lista){
         printf("%d - ", aux->elemento);
         aux = aux->proximo;
         }
-        printf("FIM");
+        printf("FIM\n\n");
     }else{
-        printf("Lista vazia!!");
+        printf("\nLista vazia!!\n");
     }
 }
 
@@ -67,13 +91,15 @@ int main(){
     No *listasimples = NULL;
     int option = -1;
     int conteudo;
+    int referencia;
 
     while (option != 0)
     {
-        printf("\t===MENU DE OPÇOES ===\n\n");
-        printf("1 -  inserir no inicio;\n");
-        printf("2 - inserir no final;\n");
-        printf("3 - mostrar lista\n");
+        printf("\n\t===MENU DE OPÇOES ===\n\n");
+        printf("1 - inserir no inicio;\n");
+        printf("2 - inserir no meio;\n");
+        printf("3 - inserir no final;\n");
+        printf("4 - mostrar lista\n");
         printf("0 - sair e fechar o programa\n");
         printf("Digite a sua opção: ");
         scanf("%d", &option);
@@ -85,15 +111,29 @@ int main(){
             scanf("%d", &conteudo);
             inserir_no_inicio(&listasimples,conteudo);
             break;
-        case 2: 
+            case 2:
+        system("clear");
+            printf("Digite um numero inteiro: ");
+            scanf("%d", &conteudo);
+        system("clear");
+            mostrar(listasimples);
+            printf("Digite um numero anterio para inserir %d: ", conteudo);
+            scanf("%d", &referencia);
+            inserir_no_meio(&listasimples, referencia, conteudo);
+            break;
+        case 3: 
         system("clear");
             printf("Digite um numero inteiro: ");
             scanf("%d", &conteudo);
             inserir_no_final(&listasimples,conteudo);
             break;
-            case 3:
+            case 4:
         system("clear");
             mostrar(listasimples);
+            break;
+            case 0:
+        system("clear");
+            printf("Saindo e fechando o programa....\n\n");
             break;
         default:
         system("clear");
